970ac68e9c4fcf884b0b82a517e029edeb3b3094
Abhi <abhichenna00@gmail.com>
2026-06-01 14:18:25 -0500

    chore(web): deploy to Cloudflare Workers as static assets

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,17 +2,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-      optimizeDeps: {
-          include: ['react', 'react-dom', 'react-dom/client', 'react/jsx-runtime'],
-      },
+	integrations: [react()],
+	vite: {
+		optimizeDeps: {
+			include: ['react', 'react-dom', 'react-dom/client', 'react/jsx-runtime'],
+		},
 	},
-
-  adapter: cloudflare(),
 })
